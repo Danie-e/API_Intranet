74de28bcefc5400a3851d04b53d58df3b9bcbb52
Daniela Eloisa <danielaeloisa1@gmail.com>
2024-06-11 20:00:52 -0300

    Adição dos quizzes

--- api/routes/index.js
+++ api/routes/index.js
@@ -5,10 +5,11 @@ import formulario from "./formularioRoutes.js";
 import respostaFormulario from "./respostasFormularioRoutes.js";
 import pdi from "./PDIRoutes.js"
 import sugestaoPdi from "./SugestoesPdiRoutes.js";
+import quiz from "./quizRoutes.js";
 
 const routes = (app) => {
     app.route("/").get((req, res) => res.status(200).send("<h1>Api criada para realização do Trabalho Interdisciplinar: Desenvolvimento de Aplicação Interativa </h1>"));
-    app.use(express.json(), usuarios, publicacao, formulario, respostaFormulario, pdi, sugestaoPdi);
+    app.use(express.json(), usuarios, publicacao, formulario, respostaFormulario, pdi, sugestaoPdi, quiz);
 };
 
 export default routes;
