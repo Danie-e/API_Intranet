209860eae74dcebcf6f4acdd0759a87a6daaa1ea
Daniela Eloisa <danielaeloisa1@gmail.com>
2024-06-11 11:25:42 -0300

    Remoção pdis

--- api/routes/index.js
+++ api/routes/index.js
@@ -3,12 +3,10 @@ import usuarios from "./usuariosRoutes.js";
 import publicacao from "./publicacaoRoutes.js"
 import formulario from "./formularioRoutes.js";
 import respostaFormulario from "./respostasFormularioRoutes.js";
-import pdi from "./PDIRoutes.js"
-import sugestoespdi from "./SugestoesPdiRoutes.js"
 
 const routes = (app) => {
     app.route("/").get((req, res) => res.status(200).send("<h1>Api criada para realização do Trabalho Interdisciplinar: Desenvolvimento de Aplicação Interativa </h1>"));
-    app.use(express.json(), usuarios, publicacao, formulario, respostaFormulario, pdi, sugestoespdi);
+    app.use(express.json(), usuarios, publicacao, formulario, respostaFormulario);
 };
 
 export default routes;
